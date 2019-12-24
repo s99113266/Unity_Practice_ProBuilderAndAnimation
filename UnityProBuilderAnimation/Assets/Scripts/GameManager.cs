d10644b069a7dc836b0fbf3c9bc31c4b8df80bd7
s99113266 <41054903+s99113266@users.noreply.github.com>
2019-12-24 19:42:41 +0800

    作業7

---
+++ UnityProBuilderAnimation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource _audioSource;
+    public AudioClip _audioClip;
+    public GameObject CC;
+    public void CC_OnClick()
+    {
+        _audioSource.PlayOneShot(_audioClip, 1f);
+        Instantiate(CC);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(CC);
+    }
+
+}
